9a1ce620b3ea5819bf15452fc7895cc11909495d
Daniel <daniel.aleksandrovych@nure.ua>
2024-04-29 21:17:05 +0300

    fix server base url

--- src/queries/clQueries/clQueries.ts
+++ src/queries/clQueries/clQueries.ts
@@ -5,7 +5,7 @@ import axios from 'axios';
 import { clQKeys } from '@/queries/clQueries/clQKeys';
 
 export const clAPI = axios.create({
-  baseURL: `${process.env.SERVER_URL}/${clQKeys.api}`,
+  baseURL: `${process.env.SERVER_URL}${clQKeys.api}`,
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
